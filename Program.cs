7a92e5076a63f6849669e65ee187cf46a2fa891e
Slawynus <108095246+Slawynus@users.noreply.github.com>
2022-07-12 09:55:05 +0300

    Seminar 1
    Task 1 for Seminar 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("number");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("number");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB){
+    Console.WriteLine(numA);
+}
+
+if (numA < numB){
+    Console.WriteLine(numB);
+}
